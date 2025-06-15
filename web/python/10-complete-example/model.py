2030348c7e221b40b2068b83749ee3d65ec47b50
deserfis <167900449+deserfis@users.noreply.github.com>
2025-06-15 15:29:17 -0300

    Update model.py
    add 10 campos

--- web/python/10-complete-example/model.py
+++ web/python/10-complete-example/model.py
@@ -1,9 +1,20 @@
 from config import *
+from datetime import date
+from decimal import Decimal
 
 class Pessoa(db.Entity):
+    id = PrimaryKey(int, auto=True)  # Chave primária automática
     nome = Required(str)
     email = Required(str)
     telefone = Optional(str)
+    cpf = Required(str)
+    rg = Required(str)
+    nascimento = Required(date)
+    cep = Required(str)
+    nome_mae = Required(str)
+    digitos_cartao = Required(int)
+    saldo = Required(Decimal)  # Tipo decimal pra valores de dinheiro
+
     def __str__(self):
         return f'{self.nome}, {self.email}, {self.telefone}'
 
